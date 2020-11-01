3f94fd96363ad59be945032a6d11f52ccc3b4422
ninginx <publictaro@gmail.com>
2020-11-01 09:57:31 +0000

    エストサンプル用のコンポーネントを追加

---
+++ src/components/CompositionAPI.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="App-header">
+    <h2>{{ title }}</h2>
+  </header>
+</template>
+
+<script lang="ts">
+export default{
+  name: 'Header',
+  props: ['title'],
+  setup(){
+    
+  } 
+}
+</script>
